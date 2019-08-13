ed97290b23331beaf11ec9ca16ccb78eb8a1bc92
Albert Yakubov <mrayakubov@hotmail.com>
2019-08-12 22:00:38 -0600

    Gallery App
    Added images to view
    polished the code
    cannot populate the view
    probably need an adapter?

--- GallegyApp/app/src/main/java/com/example/gallegyapp/MainActivity.kt
+++ GallegyApp/app/src/main/java/com/example/gallegyapp/MainActivity.kt
@@ -13,36 +13,81 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val imageViewList = mutableListOf<ImageView>()
 
 
 
+        var iV0 = ImageView(this)
+        var iV1  = ImageView(this)
+        var iV2 = ImageView(this)
+        var iV3 = ImageView(this)
+        var iV4 = ImageView(this)
+        var iV5 = ImageView(this)
+        var iV6 = ImageView(this)
+        var iV7  = ImageView(this)
+        var iV8 = ImageView(this)
+        var  iV9= ImageView(this)
 
-        imageViewList.add(0, ImageView(this))
-        imageViewList[0].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.aerial_photography_aerial_shot_aerial_view_2583847))
-        imageViewList.add(1, ImageView(this))
+        var imageViewList: Array<ImageView> = arrayOf(iV0, iV1, iV2, iV3, iV4, iV5, iV6, iV7, iV8, iV9)
+
+
+        imageViewList[0].setImageDrawable(
+            ContextCompat.getDrawable(
+                this,
+                R.drawable.aerial_photography_aerial_shot_aerial_view_2583847
+            )
+        )
+
         imageViewList[1].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.afterglow_beach_cliff_2555285))
-        imageViewList.add(2, ImageView(this))
-        imageViewList[2].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.alley_architecture_buildings_2526517))
-        imageViewList.add(3, ImageView(this))
-        imageViewList[3].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.abstract_abstract_expressionism_art_2505693))
-        imageViewList.add(4, ImageView(this))
-        imageViewList[4].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.architectural_design_architecture_bridge_2540653))
-        imageViewList.add(5, ImageView(this))
-        imageViewList[5].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.beautiful_breathtaking_canada_day_2526105))
-        imageViewList.add(6, ImageView(this))
+
+        imageViewList[2].setImageDrawable(
+            ContextCompat.getDrawable(
+                this,
+                R.drawable.alley_architecture_buildings_2526517
+            )
+        )
+
+        imageViewList[3].setImageDrawable(
+            ContextCompat.getDrawable(
+                this,
+                R.drawable.abstract_abstract_expressionism_art_2505693
+            )
+        )
+
+        imageViewList[4].setImageDrawable(
+            ContextCompat.getDrawable(
+                this,
+                R.drawable.architectural_design_architecture_bridge_2540653
+            )
+        )
+
+        imageViewList[5].setImageDrawable(
+            ContextCompat.getDrawable(
+                this,
+                R.drawable.beautiful_breathtaking_canada_day_2526105
+            )
+        )
+
         imageViewList[6].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.bloom_blossom_flora_2567011))
-        imageViewList.add(7, ImageView(this))
+
         imageViewList[7].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.close_up_colorful_colors_2529148))
-        imageViewList.add(8, ImageView(this))
-        imageViewList[8].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.clouds_coconut_trees_daylight_2486168))
-        imageViewList.add(9, ImageView(this))
-        imageViewList[9].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.adventure_automobile_classic_2533092))
+
+        imageViewList[8].setImageDrawable(
+            ContextCompat.getDrawable(
+                this,
+                R.drawable.clouds_coconut_trees_daylight_2486168
+            )
+        )
+
+        imageViewList[9].setImageDrawable(
+            ContextCompat.getDrawable(
+                this,
+                R.drawable.adventure_automobile_classic_2533092
+            )
+        )
 
 
         /* <style name="Drawable">
@@ -54,28 +99,33 @@ class MainActivity : AppCompatActivity() {
 
         </style> **/
 
+        //set up the imageView
+
 
         //TODO
         //o apply the height and width, we'll need to create a LayoutParams object
         // and pass in constants to represent match_parent and wrap_content as constructor parameters.
         //  We use LinearLayout.LayoutParams because the parent is a LinearLayout
         val layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        var i = 0
+        imageViewList.forEach {
+            it.adjustViewBounds = true
+            it.setPadding(8,8,8,8)
+            it.layoutParams = layoutParams
 
-        //populating the list
-        imageViewList.forEachIndexed{ index, imageView ->
-            imageView.layoutParams = layoutParams
-            imageView.adjustViewBounds = true
-            imageView.setPadding(8,8,8,8)
-            when {
-                index %2 != 0 -> column_left.addView(imageView)
-                else -> column_right.addView(imageView)
+            if(i++ < imageViewList.size /2){
+                column_left.addView(it)
+            }else{
+                column_right.addView(it)
             }
 
         }
 
+
+        //populating the list
     }
-
-
 }
+
+
 
 
